d479ee892569c44c2b805bf8b38d54ea2580242f
misaf <misaf.1990@gmail.com>
2026-07-14 12:34:47 +0000

    Integrate package updates into the main application

--- src/Providers/MultimediaApiServiceProvider.php
+++ src/Providers/MultimediaApiServiceProvider.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Misaf\VendraMultimediaApi\Providers;
 
+use Composer\InstalledVersions;
+
 use Illuminate\Foundation\Console\AboutCommand;
+use Illuminate\Support\Facades\Config;
 use Misaf\VendraMultimediaApi\JsonApi\V1\Server as MultimediaServer;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -19,11 +22,11 @@ final class MultimediaApiServiceProvider extends PackageServiceProvider
 
     public function packageRegistered(): void
     {
-        config()->set('jsonapi.servers.vendra-multimedia', config('jsonapi.servers.vendra-multimedia', MultimediaServer::class));
+        Config::set('jsonapi.servers.vendra-multimedia', Config::string('jsonapi.servers.vendra-multimedia', MultimediaServer::class));
     }
 
     public function packageBooted(): void
     {
-        AboutCommand::add('Vendra Multimedia API', fn() => ['Version' => 'dev-master']);
+        AboutCommand::add('Vendra Multimedia API', fn() => ['Version' => InstalledVersions::getPrettyVersion('misaf/vendra-multimedia-api')]);
     }
 }
